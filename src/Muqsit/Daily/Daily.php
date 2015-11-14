82111833ca9a397850e00cf49d9af2f2d0284dbe
Muqsit Rayyan <hackhack05@gmail.com>
2015-11-14 09:34:07 +0300

    Update Daily.php

--- src/Muqsit/Daily/Daily.php
+++ src/Muqsit/Daily/Daily.php
@@ -1,1 +1,16 @@
+<?php
+
+namespace Muqsit\Daily;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\item\Item;
+
+class Daily extends PluginBase{
+}
+
 
